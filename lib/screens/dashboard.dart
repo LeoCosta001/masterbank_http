1f51f077f2d5fe4f4a76229a531e7c7f6acdc39e
LeoCosta <leocosta.black@hotmail.com>
2022-04-28 22:33:16 -0300

    Create class _FeatureItem in dashboard

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -13,45 +13,62 @@ class Dashboard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Image.asset('assets/images/logo/masterbank_main_logo.jpg'),
-          Padding(
+          Row(
+            children: const <Widget>[
+              _FeatureItem('Transfer', Icons.monetization_on),
+              _FeatureItem('Transfer2', Icons.monetization_on),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+  final String _name;
+  final IconData _icon;
+
+  const _FeatureItem(this._name, this._icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => ContactsList(),
+              ),
+            );
+          },
+          child: Container(
             padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => ContactsList(),
-                    ),
-                  );
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(8.0),
-                  height: 100,
-                  width: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const <Widget>[
-                      Icon(
-                        Icons.people,
-                        color: Colors.white,
-                        size: 24.0,
-                      ),
-                      Text(
-                        'Contacts',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                    ],
+            height: 100,
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  _icon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  _name,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
                   ),
                 ),
-              ),
-            )
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
