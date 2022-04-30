f62d04091980454cf7a4754dd877ff656d6d58b7
LeoCosta <leocosta.black@hotmail.com>
2022-04-30 15:16:22 -0300

    Add 'http_interceptor' dependence

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,8 +1,32 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
 
 void getTransactionsList() async {
-  // Caso esteja usando um servidor local é nescessário usar o IP da mesma rede
-  final Response response = await get(Uri.parse('http://192.168.0.6:8080/transactions'));
+  final Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()]);
 
-  print(response.body);
+  // Caso esteja usando um servidor local é nescessário usar o IP da mesma rede
+  final Response response = await client.get(Uri.parse('http://192.168.0.6:8080/transactions'));
+}
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print('####### Request ######');
+    print('url: ${data.url}');
+    print('method: ${data.method}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}' );
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print('####### Response ######');
+    print('url: ${data.url}');
+    print('method: ${data.method}');
+    print('headers: ${data.headers}');
+    print('status_code: ${data.statusCode}');
+    print('body: ${data.body}' );
+    return data;
+  }
 }
