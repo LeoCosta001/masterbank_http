3ae626391348078ce5fa56f1da674b7a05b39548
LeoCosta <leocosta.black@hotmail.com>
2022-05-01 14:27:29 -0300

    Use 'FutureBuilder' in 'TransactionList' page

--- lib/screens/transactions_list.dart
+++ lib/screens/transactions_list.dart
@@ -1,38 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:masterbank/http/webclient.dart';
 import 'package:masterbank/models/transaction.dart';
+import 'package:masterbank/widgets/loading_page.dart';
 
 class TransactionsList extends StatelessWidget {
-  final List<Transaction> transactions = [];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Transactions'),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Transaction transaction = transactions[index];
-          return Card(
-            child: ListTile(
-              leading: const Icon(Icons.monetization_on),
-              title: Text(
-                transaction.value.toString(),
-                style: const TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(
-                transaction.contact.accountNumber.toString(),
-                style: const TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          );
+      appBar: AppBar(title: const Text('Transactions')),
+      body: FutureBuilder<List<Transaction>>(
+        initialData: const [],
+        future: getTransactionsList(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return LoadingPage();
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              if (snapshot.data != null) {
+                final List<Transaction> transactions = snapshot.data as List<Transaction>;
+                return ListView.builder(
+                  itemBuilder: (context, index) {
+                    final Transaction transaction = transactions[index];
+                    return Card(
+                      child: ListTile(
+                        leading: const Icon(Icons.monetization_on),
+                        title: Text(
+                          transaction.value.toString(),
+                          style: const TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        subtitle: Text(
+                          transaction.contact.accountNumber.toString(),
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: transactions.length,
+                );
+              }
+
+              return const Text('Unknown error!');
+          }
+          return Container();
         },
-        itemCount: transactions.length,
       ),
     );
   }
